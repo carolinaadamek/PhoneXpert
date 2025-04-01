e03b08fbf55c7e887921dec6d110f9aff6cedcbe
Chris <wi23b024@technikum-wien.at>
2025-04-01 20:06:10 +0200

    initial commit

---
+++ config/db.php
@@ -0,0 +1,11 @@
+<?php
+$host = "localhost";
+$dbname = "phonexpert_webshop";
+$user = "root";
+$password = "";
+
+$conn = new mysqli($host, $user, $password, $dbname);
+if ($conn->connect_error) {
+    die("Verbindung fehlgeschlagen: " . $conn->connect_error);
+}
+?>
